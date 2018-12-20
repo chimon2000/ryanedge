60b38ddd9beee7ed90c3b53065aaff13e5d8ba76
Ryan <ryan.c.edge@outlook.com>
2018-12-20 15:39:13 -0500

    Fixed dev only when local

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-require('ts-node/register')
-require('./src/index')
+const fs = require('fs')
+const path = require('path')
+const project = path.join(__dirname, '../tsconfig.json')
+const isDev = fs.existsSync(project)
+
+require(`../${isDev ? 'src' : 'lib'}`)
