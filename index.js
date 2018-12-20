80a37bbc78c20c843b0f72c4ba0b1b4cb877a22f
Ryan <ryan.c.edge@outlook.com>
2018-12-20 16:04:22 -0500

    Fixed stuff

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const path = require('path')
 const project = path.join(__dirname, '../tsconfig.json')
 const isDev = fs.existsSync(project)
 
-require(`../${isDev ? 'src' : 'lib'}`)
+require(`./${isDev ? 'src' : 'lib'}`)
